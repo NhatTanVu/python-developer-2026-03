c58bf3a8a4b2f8cef4adf509cb89acb69a8b83c0
Nhat Tan Vu <onchua2006@gmail.com>
2026-03-24 20:16:15 -0700

    Format post body as Markdown

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.utils.safestring import mark_safe
 
 from blog.models import Post
+import markdown
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -26,8 +28,16 @@ def welcome(request: HttpRequest) -> HttpResponse:
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
+    post_body_html = markdown.markdown(
+        post.body,
+        extensions=["extra", "fenced_code", "tables"],
+    )
+
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {
+            "post": post,
+            "post_body_html": mark_safe(post_body_html)
+        },
     )
